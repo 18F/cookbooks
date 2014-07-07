64df766399c96dfc61fc326d69e6a810b3ea15f8
DaveCaraway <dave@fogmine.com>
2014-07-07 16:03:29 -0400

    Adding Berkshelf integration, ignoring .vagrant and tweaking the defaults

--- codetalker/recipes/default.rb
+++ codetalker/recipes/default.rb
@@ -1,22 +1,3 @@
-#
-# Cookbook Name:: codetalker
-# Recipe:: default
-#
-# Copyright 2014, Example Com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 user node[:codetalker][:user] do
   action :create
   system true
@@ -32,19 +13,19 @@ directory node[:codetalker][:deploy_dir] do
 end
 
 git node[:codetalker][:deploy_dir] do
-   repository "https://github.com/18F/codetalker.git"
-   reference "master"
+   repository node[:codetalker][:git_repo]
+   reference node[:codetalker][:git_branch]
    action :sync
 end
 
-file node[:codetalker][:librarian_script] do
-	mode "700"
-end
+# file node[:codetalker][:librarian_script] do
+# 	mode "700"
+# end
 
-execute "bash #{node[:codetalker][:librarian_script]}" do
-  user "root"
-end
+# execute "bash #{node[:codetalker][:librarian_script]}" do
+#   user "root"
+# end
 
-execute "puppet apply #{node[:codetalker][:manifest]}" do
-  user "root"
-end
+# execute "puppet apply #{node[:codetalker][:manifest]}" do
+#   user "root"
+# end
