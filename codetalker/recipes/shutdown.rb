9e285a9c98ebce327cc10b70f11dc9d42b0e6ab0
DaveCaraway <dave@fogmine.com>
2014-07-17 15:21:16 -0400

    Added shutdown recipe

---
+++ codetalker/recipes/shutdown.rb
@@ -0,0 +1,6 @@
+execute "Stop the nodejs daemon" do
+  command "pm2 stop -u #{node[:codetalker][:user]} codetalker"
+  environment ({'HOME' => node[:codetalker][:user_home] })
+  user node[:codetalker][:user]
+  cwd node[:codetalker][:deploy_dir]
+end
